4952762c0742b76f1563cbc60d27ce2aaf7da100
Seniyax <151843498+Seniyax@users.noreply.github.com>
2024-12-21 16:13:11 +0530

    Update businessController.js
    created the apis for update delete and find

---
+++ src/controllers/businessController.js
@@ -0,0 +1,65 @@
+const { request } = require("express");
+
+
+export const updateBusiness = async(req,res) => {
+    const{name,role} = req.body;
+
+    const{data,error} = await supabase
+
+    .from('business')
+    .update({name,role})
+    .eq();
+
+    if(error){
+       return res.status(404).json({message:"failed to update the requet"});
+    }
+
+   return res.status(200).json({message:"updated",data});
+};
+
+export const removeBusiness = async (req,res) => {
+    const {BusinessID} = req.params;
+
+    const {data,error} = await supabase
+
+    .from('business')
+    .delete()
+    .eq('id',id);
+
+    if(error){
+      return  res.status(404).json({message:"failed to delete the customer"})
+    }
+
+    return res.status(200).json({message:"Deleted the business",data})
+};
+
+export const findBusinessById = async (req,res) => {
+
+    const {BusinessID} = req.params;
+
+    const {data,error} = await supabase
+
+    .from('business')
+    .select('*')
+    .eq('id',BusinessID);
+
+    if(error){
+        return res.status(400).json({message:"Id not found"});
+    }
+
+    return res.status(200).json(data[0]);
+    
+    
+};
+
+ export const findBusinessByAll = async (req,res) => {
+
+    const{data,error} = await supabase.from('business').select('*');
+
+    if(error){
+        return res.status(400).json({message:"couldn't find"});
+    }
+
+    return res.status(200).json(data);
+    
+};
