3596e1c2295c217da38681291558537df83ba7a1
Seniyax <151843498+Seniyax@users.noreply.github.com>
2025-02-13 10:09:18 +0530

    created and updated

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const Router = express.Router();
+const authController = require('');
+
+router.post('/signup')
+router.post('/signin')
+
+module.exports = router;
