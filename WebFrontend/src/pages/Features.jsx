fa1691c6eb49b8ac091ff632b1726222fbd24c6b
Nishanjali Kamalendran <nishanjali.kamal@gmail.com>
2025-02-14 23:43:57 +0530

    Create Features.jsx
    Updated the basic structure of the features page

---
+++ WebFrontend/src/pages/Features.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../style/Features.css";
+function Features() {
+    return (
+        <div>
+            <h1>BuyBooster's Best Features</h1>
+            <ul>
+                <li>Write here a key feature of the app or software that is being advertised here.</li>
+                <li>Write here a key feature of the app or software that is being advertised here.</li>
+                <li>Write here a key feature of the app or software that is being advertised here.</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Features;
