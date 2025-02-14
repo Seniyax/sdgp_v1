c39e776b16e1ba78acaf9cbbd6fec2d61719a70d
Nishanjali Kamalendran <nishanjali.kamal@gmail.com>
2025-02-14 23:40:33 +0530

    Update Navbar.jsx

--- WebFrontend/src/components/Navbar.jsx
+++ WebFrontend/src/components/Navbar.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../style/Navbar.css";
 
+function Navbar() {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/features">Features</Link>
+            <Link to="/testimonials">Testimonials</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
