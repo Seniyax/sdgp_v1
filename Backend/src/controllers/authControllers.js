104fee45a786f9ea3290cccd3fa7cb95413f1a01
Seniyax <151843498+Seniyax@users.noreply.github.com>
2025-02-16 17:32:53 +0530

    authcontroller
    updated the authcontroller and created validate.js

--- Backend/src/controllers/authControllers.js
+++ Backend/src/controllers/authControllers.js
@@ -1,4 +1,6 @@
 const User = require('');
+const {validateEmail} = require("utils\validate.js");
+const jwt = require('jsonwebtokens')
 
 exports.signup = async(req,res,)=>
 {
@@ -7,10 +9,34 @@ exports.signup = async(req,res,)=>
 
         if(!email||!password||!name){
             return res.status(400).json({
+                status : 'error',
                 message:"Please provide all the required details"
 
             });
         }
+
+        if (!validateEmail(email)){
+            return res.status(400).json({
+                status:'error',
+                message:'please provide a valid email'
+            });
+        }
+        
+        // checking whether a user is alerady exist
+        const checkExistingUser = await User.findOne({email});
+        if(checkExistingUser){
+            return res.status(400).json({
+                status:'error',
+                message:'Email already in use'
+            });
+        }
+
+        //create a new user
+        const user = await User.create({
+            email,
+            password,
+            name
+        });
     }
 
     catch(error){
