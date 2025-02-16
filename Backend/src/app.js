f49ed526c84c953141ff5fbe3972bcb18ac5550e
Uvindu Amaratunga <83293493+UvinduRokz@users.noreply.github.com>
2025-02-16 14:43:51 +0530

    Added business CRUD routes for web backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const businessRoutes = require("./routes/businessRoutes");
 
 const app = express();
 
 // Middleware
 app.use(cors()); // Enable CORS for frontend requests
 app.use(express.json()); // Parse JSON requests
+
+//routes
+app.use("/api/business", businessRoutes);
 
 module.exports = app; // Export the configured app
