7ebf3ffc5f4862830cf4444920dfcda24f535514
Seniyax <151843498+Seniyax@users.noreply.github.com>
2025-03-10 17:28:20 +0530

    ESM to commonjs

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,4 @@
-const app = require('./src/app'); // Import the configured app
+import app from './src/app'; // Import the configured app
 const PORT = process.env.PORT || 3000;
 
 // Start the server
