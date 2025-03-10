98a99ef1a12a4684dc2e1c4b6105b7facd57e892
Uvindu Amaratunga <83293493+UvinduRokz@users.noreply.github.com>
2025-03-10 10:33:12 +0530

    deleted: slot routes and business logic. added: floor plan routes and controller

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const businessRoutes = require("./routes/businessRoutes");
 const emailRoutes = require("./routes/emailRoutes");
 const userRoutes = require("./routes/userRoutes");
 const bHURoutes = require("./routes/bHURoutes");
-const slotRoutes = require("./routes/slotRoutes");
 const mLRoutes = require("./routes/mLRoutes");
+const floorPlanRoutes = require("./routes/floorPlanRoutes");
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use("/api/business", businessRoutes);
 app.use("/api/email", emailRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/business-user-relation", bHURoutes);
-app.use("/api/slot", slotRoutes);
 app.use("/api/ml", mLRoutes);
+app.use("/api/floor-plan", floorPlanRoutes);
 
 module.exports = app; // Export the configured app
---
+++ src/controllers/floorPlanController.js
@@ -0,0 +1,15 @@
+exports.getFloorPlan = async (req, res) => {
+
+};
+
+exports.createFloorPlan = async (req, res) => {
+
+};
+
+exports.updateFloorPlan = async (req, res) => {
+
+};
+
+exports.deleteFloorPlan = async (req, res) => {
+    
+};
