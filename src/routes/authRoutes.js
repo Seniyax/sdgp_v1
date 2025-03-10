a965ede5371f3fae7858cecba5d99eda26ee9ea0
Seniyax <151843498+Seniyax@users.noreply.github.com>
2025-03-10 11:38:48 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const Router = express.Router();
+
 const authController = require('../controllers/authControllers');
 
+const router = express.Router();
 
+router.post('/signup',authController.signup)
+router.post('/signin',authController.signin)
 
-router.post('/signup')
-router.post('/signin')
+router.post('/social/:provider',authController.socialLogin)
 
-
+router.post('/logout',authController.logout)
 
 module.exports = router;
