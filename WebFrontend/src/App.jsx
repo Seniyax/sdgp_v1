ba43d5a89fbc8e65dbdfd366a9f975a3d665c298
Nishanjali Kamalendran <nishanjali.kamal@gmail.com>
2025-03-14 10:57:07 +0530

    routes changed

--- WebFrontend/src/App.jsx
+++ WebFrontend/src/App.jsx
@@ -6,6 +6,16 @@ import {
   useLocation,
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import SignIn from './pages/Signin.jsx';
+import SignUp from './pages/Signup.jsx';
+import Passwordconfirmation from './pages/Passwordconfirmation.jsx';
+import Businessform from './pages/Businessform.jsx';
+import './App.css';
+import Dashboard from "../src/pages/Dashboard.jsx";
+import Verification from "../src/pages/Verification.jsx";
 // import Home from "./pages/Home";
 // import SignIn from "./pages/Signin.jsx";
 // import SignUp from "./pages/Signup.jsx";
@@ -22,21 +32,21 @@ const AppContent = () => {
   // Hide the Navbar on any route that starts with "/floorplan"
   const hideNavbar = location.pathname.startsWith("/floorplan");
 
+  const noNavbarRoutes = ["/signin", "/signup", "/passwordconfirmation", "/businessform"];
+
   return (
     <>
-      {!hideNavbar && <Navbar />}
+      {!noNavbarRoutes.includes(location.pathname) && <Navbar />}
+     
       <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route
-          path="/passwordconfirmation"
-          element={<Passwordconfirmation />}
-        />
-        <Route path="/businessform" element={<Businessform />} />
-         <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/verification" element={<Verification />} /> */}
+        <Route path="/" element={<Home />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/verification" element={<Verification />} />
+                <Route path="/businessform" element={<Businessform />} />
         <Route path="/floorplan" element={<FloorPlanDesigner />} />
+      
       </Routes>
     </>
   );
