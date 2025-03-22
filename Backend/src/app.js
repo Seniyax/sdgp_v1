cff913642672892680f4b49b524f788d13104083
Uvindu Amaratunga <83293493+UvinduRokz@users.noreply.github.com>
2025-03-22 18:45:42 +0530

    Revert "updated controllers and routes"
    This reverts commit b25936ec461832f044edaf2bd25ae6fa067353b7.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,6 @@
 const express= require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes")
-const authers = require("./controllers/authControllers")
 const app = express();
 
 // Middleware
@@ -9,10 +8,5 @@ app.use(cors()); // Enable CORS for frontend requests
 app.use(express.json()); // Parse JSON requests
 
 app.use('/auth',authRoutes)
-app.use(cookieParser());
-
-// very protected api
-app.get('/profile',authers.requireAuth,profileController.getProfile);
-
 
 module.exports = app; // Export the configured app
--- Backend/src/config/supabaseClient.js
+++ Backend/src/config/supabaseClient.js
@@ -4,12 +4,6 @@ require('dotenv').config();
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY,{
-    auth:{
-        autoRefreshToken:true,
-        persistSession:true,
-        detectSessionInUrl:true
-    }
-});
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 module.exports = supabase;
